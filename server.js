4ec859bec83b544eef1601e6a81d5731a4476c4b
Nekhoroshkin Ruslan <nekhoroshkin.ruslan@gmail.com>
2015-07-31 07:35:42 +0300

    Completed server side

---
+++ server.js
@@ -0,0 +1,34 @@
+
+var express = require('express');
+var path = require('path');
+var objectService = require('./object_service');
+var rs = require('./render_services');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+//routes
+app.get('/:object_name_url' , function(req, res){
+    var objectNameUrl = req.params.object_name_url;
+
+    objectService.getObject(objectNameUrl, function(err, object){
+        if(err){
+            res.status(404);
+            res.render('error', {pageTitle: 'Error'});
+            return;
+        }
+
+        res.render('index', {title: 'Object', renderedObject: rs.renderObject(object)});
+    });
+});
+
+var port = 3000;
+app.listen(port, function(){
+    console.log('Listening on port: ' + port);
+});
+
